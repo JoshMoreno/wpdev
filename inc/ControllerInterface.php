8ab9905b4c71e0db0e8f60a985ecc330380a1f96
Josh Moreno <Josh@GaslampMedia.com>
2018-01-22 06:55:18 -0800

    initial ControllerInterface

---
+++ inc/ControllerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WPDev;
+
+interface ControllerInterface
+{
+	/**
+	 * This is the data that will get exposed to the view.
+	 * It will be extracted into variables - extract().
+	 *
+	 * @return array
+	 */
+	public function build(): array;
+}
