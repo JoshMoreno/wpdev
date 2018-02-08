7825fb63a70509cbe6f9e122ce764c58e429445e
Josh Moreno <Josh@GaslampMedia.com>
2018-02-08 07:11:31 -0800

    initial addition of PostType facade to docs

--- docs/parser.php
+++ docs/parser.php
@@ -7,3 +7,9 @@ foreach ($loader->getClassMap() as $class => $path) {
         generate_yaml_file($class, __DIR__.'/_data/models/');
     }
 }
+
+foreach ($loader->getClassMap() as $class => $path) {
+	if (\Stringy\Stringy::create($class)->startsWith('WPDev\\Facades')) {
+		generate_yaml_file($class, __DIR__.'/_data/facades/');
+	}
+}
