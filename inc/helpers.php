af2354db7b84041ba4105cb86b1fff2cadc30ef1
Josh Moreno <josh@joshmoreno.com>
2018-02-08 21:38:55 -0800

    add helper fn to map wp query posts to our Post model

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,18 +1,40 @@
 <?php
 
 use WPDev\Debug\Dumper;
+use WPDev\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
 | Dump and Die
 |--------------------------------------------------------------------------
 */
-if (!function_exists('dd')) {
-    function dd(...$args) {
+if ( ! function_exists('dd')) {
+    function dd(...$args)
+    {
         foreach ($args as $arg) {
             Dumper::dump($arg);
         }
 
         die();
+    }
+}
+
+/**
+ * Map WP_Query->posts to WPDev\Models\Post[]
+ *
+ * @param $wp_query
+ *
+ * @return array
+ */
+if ( ! function_exists('get_posts_from_query')) {
+    function get_posts_from_query($wp_query)
+    {
+        if ( ! $wp_query instanceof WP_Query) {
+            return [];
+        }
+
+        return array_map(function ($post) {
+            return new Post($post);
+        }, $wp_query->posts);
     }
 }
