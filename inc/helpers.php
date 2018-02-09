827a41982d732b5197c69d0245bad729587a7f2b
Josh Moreno <josh@joshmoreno.com>
2018-02-08 22:46:06 -0800

    added helpers docs

--- inc/helpers.php
+++ inc/helpers.php
@@ -3,12 +3,12 @@
 use WPDev\Debug\Dumper;
 use WPDev\Models\Post;
 
-/*
-|--------------------------------------------------------------------------
-| Dump and Die
-|--------------------------------------------------------------------------
-*/
 if ( ! function_exists('dd')) {
+    /**
+     * Dump and die
+     *
+     * @param mixed ...$args Items to dump
+     */
     function dd(...$args)
     {
         foreach ($args as $arg) {
@@ -19,20 +19,16 @@ if ( ! function_exists('dd')) {
     }
 }
 
-/**
- * Map WP_Query->posts to WPDev\Models\Post[]
- *
- * @param $wp_query
- *
- * @return array
- */
 if ( ! function_exists('get_posts_from_query')) {
-    function get_posts_from_query($wp_query)
+    /**
+     * Map WP_Query->posts to WPDev\Models\Post objects
+     *
+     * @param \WP_Query $wp_query A WP_Query object
+     *
+     * @return array If not empty will be an array of WPDev\Models\Post objects
+     */
+    function get_posts_from_query(WP_Query $wp_query)
     {
-        if ( ! $wp_query instanceof WP_Query) {
-            return [];
-        }
-
         return array_map(function ($post) {
             return new Post($post);
         }, $wp_query->posts);
