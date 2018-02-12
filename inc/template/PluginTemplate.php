9046827b2021612f4be532e8e42ed8fa3ed4a3bb
Josh Moreno <josh@joshmoreno.com>
2018-02-11 23:06:54 -0800

    initial template loaders

---
+++ inc/template/PluginTemplate.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace WPDev\Template;
+
+class PluginTemplate extends Template
+{
+    protected $filePath;
+    protected $pluginFolder;
+
+    /**
+     * Locates and renders a theme template. Falls back to the absolute `$file_path` passed in.
+     *
+     * @param string $file_path The absolute path to the file in the plugin. The basename will be used when searching in the themes.
+     * @param array $data Data to be passed to the template.
+     *
+     */
+    public function __construct(string $file_path, array $data = [])
+    {
+        $this->filePath = $file_path;
+        $this->pluginFolder   = $this->pluginFolderName($file_path);
+        parent::__construct($file_path, $data);
+
+    }
+
+    public function includeTemplate()
+    {
+        if (!parent::includeTemplate()) {
+            $data = $this->data;
+            extract($data);
+            include $this->filePath;
+        }
+    }
+
+    protected function paths()
+    {
+        return [
+            get_stylesheet_directory()."/templates/plugins/{$this->pluginFolder}",
+            get_template_directory()."/templates/plugins/{$this->pluginFolder}",
+        ];
+    }
+
+    /**
+     * Gets the plugin folder name containing the template file.
+     *
+     * We use this to help avoid template naming conflicts between plugins and themes.
+     * Also has the added benefit of keeping the project organized.
+     *
+     * @param $file_path
+     */
+    protected function pluginFolderName($file_path)
+    {
+        $relative_plugin_path = str_replace(WP_PLUGIN_DIR.'/', '', $file_path);
+        $file_path_array      = explode('/', $relative_plugin_path);
+
+        return $file_path_array[0];
+    }
+}
---
+++ inc/template/Template.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace WPDev\Template;
+
+use Symfony\Component\Finder\Finder;
+
+class Template
+{
+    protected $data;
+    protected $fileName;
+    protected $paths;
+    protected $foundTemplates = [];
+
+    /**
+     * Constructor. Alternatively use `Template::include()` or `Template::locate()`.
+     *
+     * @param string $file_name
+     * @param array $data
+     */
+    public function __construct(string $file_name, array $data = [])
+    {
+        $this->fileName       = basename($file_name);
+        $this->data           = $data;
+        $this->paths          = $this->buildValidPaths();
+        $this->foundTemplates = $this->locateAllTemplates();
+    }
+
+    /**
+     * For a more fluid syntax. Alternatively use `Template::include()` or `Template::locate()`.
+     *
+     * @param string $file_name
+     * @param array $data
+     *
+     * @return $this
+     */
+    public static function create(string $file_name, array $data = [])
+    {
+        return new static($file_name, $data);
+    }
+
+    public function getTemplate()
+    {
+        if ( ! $this->foundTemplates) {
+            return '';
+        }
+
+        /** @var \Symfony\Component\Finder\SplFileInfo $file */
+        $file = reset($this->foundTemplates); // grab the first file
+
+        return $file->getRealPath();
+    }
+
+    /**
+     * Include a theme template file. Optionally pass data.
+     *
+     * @param string $file_name The file name of the template.
+     * @param array $data Data to be passed to view. Will also be extracted into variables.
+     *
+     * @return bool True if successfully included the template. Otherwise, false.
+     */
+    public static function include(string $file_name, array $data = [])
+    {
+        return static::create($file_name, $data)->includeTemplate();
+    }
+
+    /**
+     * Includes the template file passing along the data.
+     *
+     * @return bool True if successfully included the template. Otherwise, false.
+     */
+    public function includeTemplate()
+    {
+        if (!$template = $this->getTemplate()) {
+            return false;
+        }
+
+        $data = $this->data;
+        extract($data);
+        include $this->getTemplate();
+
+        return true;
+    }
+
+    /**
+     * Locates a template file.
+     *
+     * @param string $file_name
+     *
+     * @return string The path to the template file. Empty if none found.
+     */
+    public static function locate(string $file_name)
+    {
+        return self::create($file_name)->getTemplate();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Protected
+    |--------------------------------------------------------------------------
+    */
+
+    protected function buildValidPaths()
+    {
+        $paths = array_unique($this->paths());
+
+        $real_paths = array_filter($paths, function ($path) {
+            return file_exists($path) && is_dir($path);
+        });
+
+        return $real_paths;
+    }
+
+    protected function locateAllTemplates()
+    {
+        dump($this->paths, $this->fileName);
+        if (!$this->paths) {
+            return [];
+        }
+
+        $finder    = new Finder();
+        $templates = $finder->files()->in($this->paths)->name($this->fileName);
+
+        return iterator_to_array($templates);
+    }
+
+    /**
+     * Defines the paths to look for templates in.
+     *
+     * @return array
+     */
+    protected function paths()
+    {
+        return [
+            get_stylesheet_directory().'/templates',
+            get_template_directory().'/templates',
+        ];
+    }
+}
