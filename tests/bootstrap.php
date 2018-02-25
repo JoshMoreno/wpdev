ecacf46522c2537e6bb9fede26696199af537ec0
Josh Moreno <josh@joshmoreno.com>
2018-02-24 19:58:33 -0800

    update test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,7 @@
  * @package Wpdev
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}
+$_tests_dir = dirname(__DIR__).'/test-env/wordpress-tests-lib';
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
@@ -23,7 +19,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/wpdev.php';
+	require dirname( __DIR__ ) . '/wpdev.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
