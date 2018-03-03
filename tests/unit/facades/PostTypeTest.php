78c450b1f0056fd354ed45e62112b8f75b0a9944
Josh Moreno <josh@joshmoreno.com>
2018-03-03 09:23:06 -0800

    refacor PostType name unit tests

--- tests/unit/facades/PostTypeTest.php
+++ tests/unit/facades/PostTypeTest.php
@@ -4,46 +4,75 @@ namespace WPDev\Tests\Unit;
 use InvalidArgumentException;
 use WP_Mock\Tools\TestCase;
 use WPDev\Facades\PostType;
+use WPDev\Models\Post;
 
 class PostTypeTest extends TestCase {
+    protected $postType;
+
+    /*
+    |--------------------------------------------------------------------------
+    | Setup and Teardown
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * This will run before each test
+     */
+    public function setUp()
+    {
+        $this->postType = PostType::create('project');
+    }
+
+    /**
+     * This will run after each test
+     */
+    public function tearDown()
+    {
+        $this->postType = null;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tests
+    |--------------------------------------------------------------------------
+    */
+
     /**
      * Tests the post type name validator
      *
-     * @param $name
+     * @param $name PostType name
      *
      * @dataProvider validNameProvider
      */
-    public function testNameValidatesCorrectly($name)
+    public function testSingularNameValidatesCorrectly($name)
     {
         $this->expectException(InvalidArgumentException::class);
         PostType::create($name);
     }
 
-    public function testSingularNameGeneratesCorrectly()
+    /**
+     * @param $name PostType name
+     * @param $expected_singular_name
+     *
+     * @dataProvider nameProvider
+     */
+    public function testSingularNameGeneratesCorrectly($name, $expected_singular_name)
     {
-        $names = [
-            // capitalizes
-            'project'           => 'Project',
-            //turns underscores into spaces and capitalizes
-            'dummy_name'        => 'Dummy Name',
-            //turns underscores into spaces and capitalizes
-            'dummy_sample_name' => 'Dummy Sample Name',
-            // capitalizes hyphenated words
-            'dummy-name'        => 'Dummy-Name',
-            // capitalizes hyphenated words
-            'dummy-sample-name' => 'Dummy-Sample-Name',
-            // handles both spaces and hyphenated fine
-            'dummy_sample-name' => 'Dummy Sample-Name',
-            // doesn't mess with last hyphen, edge case
-            'dummy-sample-'     => 'Dummy-Sample-',
-            // trims spaces
-            '_dummy_sample_'    => 'Dummy Sample',
-        ];
+        $postType = PostType::create($name);
+        $this->assertEquals($expected_singular_name, $postType->singularName);
+    }
 
-        foreach ($names as $name => $expected) {
-            $postType = PostType::create($name);
-            $this->assertEquals($expected, $postType->singularName);
-        }
+    /**
+     * @param $name PostType name
+     * @param $expected_singular_name
+     * @param $expected_plural_name
+     *
+     * @dataProvider nameProvider
+     */
+    public function testPluralNameGeneratesCorrectly($name, $expected_singular_name, $expected_plural_name)
+    {
+        $postType = PostType::create($name);
+        $this->assertEquals($expected_plural_name, $postType->pluralName);
     }
 
     public function testSetArg()
@@ -95,22 +124,27 @@ class PostTypeTest extends TestCase {
     |--------------------------------------------------------------------------
     */
 
-    /**
-     * Valid names
-     *
-     * @return array
-     */
-    public function validNameProvider()
+    public function nameProvider()
     {
-        $tests = [
-            'empty'                    => [''],
-            'contains spaces'          => ['some name'],
-            'contains only spaces'     => [' '],
-            'contains capital letters' => ['Somename'],
-            'more than 20 characters'  => ['123456789123456789123'],
+        // 'test name' => ['PostType name', 'Expected singular', 'Expected plural']
+        return [
+            // capitalizes
+            'project'           => ['project', 'Project', 'Projects'],
+            //turns underscores into spaces and capitalizes
+            'dummy_name'        => ['dummy_name', 'Dummy Name', 'Dummy Names'],
+            //turns underscores into spaces and capitalizes
+            'dummy_sample_name' => ['dummy_sample_name', 'Dummy Sample Name', 'Dummy Sample Names'],
+            // capitalizes hyphenated words
+            'dummy-name'        => ['dummy-name', 'Dummy-Name', 'Dummy-Names'],
+            // capitalizes hyphenated words
+            'dummy-sample-name' => ['dummy-sample-name', 'Dummy-Sample-Name', 'Dummy-Sample-Names'],
+            // handles both spaces and hyphenated fine
+            'dummy_sample-name' => ['dummy_sample-name', 'Dummy Sample-Name', 'Dummy Sample-Names'],
+            // doesn't mess with last hyphen, edge case
+            'dummy-sample-'     => ['dummy-sample-', 'Dummy-Sample-', 'Dummy-Sample-s'],
+            // trims spaces
+            '_dummy_sample_'    => ['_dummy_sample_', 'Dummy Sample', 'Dummy Samples'],
         ];
-
-        return array_merge($this->reservedNamesProvider(), $tests);
     }
 
     /**
@@ -133,5 +167,23 @@ class PostTypeTest extends TestCase {
             'reserved name of order'               => ['order'],
             'reserved name of theme'               => ['theme'],
         ];
+    }
+
+    /**
+     * Valid names
+     *
+     * @return array
+     */
+    public function validNameProvider()
+    {
+        $tests = [
+            'empty'                    => [''],
+            'contains spaces'          => ['some name'],
+            'contains only spaces'     => [' '],
+            'contains capital letters' => ['Somename'],
+            'more than 20 characters'  => ['123456789123456789123'],
+        ];
+
+        return array_merge($this->reservedNamesProvider(), $tests);
     }
 }
